c3a8d549a26a019e215e6b6becaaca33ed1ead73
tiliavir <tiliavir@users.noreply.github.com>
2025-12-16 18:42:11 +0100

    ci: local validation
    Signed-off-by: tiliavir <tiliavir@users.noreply.github.com>

--- validate.mjs
+++ validate.mjs
@@ -1,21 +1,13 @@
 'use strict';
 
 import glob from "glob-all";
-import {w3cHtmlValidator} from "w3c-html-validator";
+import vnuJar from 'vnu-jar';
 
-
-glob([
-      'public/**/*.html',
-      '!public/google*.html',
-      '!public/amp/**'],
+await glob([
+        'public/**/*.html',
+        '!public/google*.html',
+        '!public/amp/**'],
     async (err, files) => {
-      const promises = files.map(file => w3cHtmlValidator.validate({filename: file}));
-
-      let results = await Promise.all(promises);
-
-      results.forEach(w3cHtmlValidator.reporter);
-      if (!results.reduce((p, c) => p && c.validates, true)) {
-        throw new Error('Validation failed!');
-      }
+        await vnuJar.vnu.check(files, {});
     }
 );
