95d07805caa675ee8b457a3fbfdf62f4aa2ada92
tiliavir <tiliavir@users.noreply.github.com>
2026-02-16 22:16:02 +0100

    fix: validation
    Signed-off-by: tiliavir <tiliavir@users.noreply.github.com>

--- validate.mjs
+++ validate.mjs
@@ -6,8 +6,15 @@ import vnuJar from 'vnu-jar';
 await glob([
         'public/**/*.html',
         '!public/google*.html',
+        'public/**/*.css',
         '!public/amp/**'],
     async (err, files) => {
-        await vnuJar.vnu.check(files, {});
+        await vnuJar.vnu.check([
+            "--also-check-svg",
+            "--also-check-css",
+            "--filterpattern",
+            ".*view-transition.*|.*CSS: Parse Error.*",
+            ...files
+        ], {});
     }
 );
